2d6cfe02609c415d1773221fedf52ff9a61f85ae
Jhonny Brito <jhonnywobrito@gmail.com>
2025-01-03 15:52:58 -0300

    Corrigido transição entre jogadores (modal)

--- components/script.js
+++ components/script.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'prefeito': 'img/prefeito.png',
         'guarda-costas': 'img/guarda-costas.png',
         'detetive': 'img/detetive.png',
-        'portador do amuleto': 'img/portador-d0-amuleto.png',
+        'portador do amuleto': 'img/portador-do-amuleto.png',
         'vidente de aura': 'img/vidente-de-aura.png',
         'príncipe bonitão': 'img/principe-bonitao.png',
         'maçom': 'img/macom.png',
@@ -106,6 +106,51 @@ document.addEventListener('DOMContentLoaded', () => {
         'lobo solitário': 'img/lobo-solitario.png',
         'filhote de lobisomem': 'img/filhote-lobisomem.png'
     };
+
+    const descricaoPorPapel = {
+        "aldeão": "Durante o dia, você discute com a aldeia sobre quem pode ser o lobisomem e decide alguém para matar.",
+        "vidente": "Toda noite você poderá descobrir o papel de outro jogador.",
+        "médico": "Durante a noite, você acorda e seleciona um jogador que não poderá ser morto pelos lobisomens naquela noite. Você não poderá proteger o mesmo jogador duas noites seguidas.",
+        "caçador": "Quando você morrer poderá escolher outra pessoa para morrer com você.",
+        "bruxa": "Você tem duas opções que podem ser usadas durante a noite: uma que irá salvar outro jogador de ser morto pelos lobisomens e um veneno que irá matar outro jogador.",
+        "aprendiz de vidente": "Se o vidente morrer, você tomará seu lugar e começará a procurar por lobisomens.",
+        "pacifista": "Uma vez por jogo você pode revelar o papel de um jogador para todos e evitar qualquer pessoa de votar durante aquele dia.",
+        "sacerdote": "Você poderá usar a água benta em outro jogador. Se esse jogador for um lobisomem, ele morre. Se não, você morre. Só pode ser usada uma única vez.",
+        "prefeito": "Se você revelar seu papel para a aldeia, seu voto conta duas vezes durante o dia.",
+        "guarda-costas": "Durante a noite, você poderá selecionar um jogador para proteger. Se os lobisomens tentarem matar esse jogador, você morre no lugar.",
+        "detetive": "Cada noite você pode selecionar dois jogadores para ver se eles pertencem ao mesmo time.",
+        "portador do amuleto": "Você não poderá ser morto por lobisomens durante a noite.",
+        "vidente de aura": "Toda noite você seleciona um jogador para saber se ele é o lobisomem.",
+        "príncipe bonitão": "A primeira vez que a aldeia tentar te matar na votação, você mostra seu papel e sobrevive.",
+        "maçom": "Você pode ver quem são os outros maçons.",
+        "menininha": "Se você abrir seus olhos, há 20% de chance de detectar um lobisomem; mas também há 5% de chance dos lobisomens te encontrarem e te matarem.",
+        "cientista maluco": "Você é um aldeão normal, exceto que quando você morrer uma substância tóxica será liberada e ela irá matar as duas pessoas ao seu lado.",
+        "caçador de cabeças": "Seu objetivo é fazer seu alvo ser linchado pela vila durante o dia. Se o seu alvo morrer de outra maneira, você irá virar um aldeão normal.",
+        "humano leproso": "Quando você é morto pelos lobisomens, eles não serão capazes de matar na próxima noite.",
+        "valentão": "Quando atacado pelos lobisomens, você sobrevive até o próximo dia.",
+        "menino travesso": "Apenas uma vez por jogo você pode trocar os papéis de dois jogadores.",
+        "prostituta": "Durante a noite você pode ficar na casa de outro jogador. Se esse jogador for um lobisomem ou for morto por um, você também morrerá. Se os lobisomens tentarem te matar e você estiver na casa de outra pessoa, você sobrevive.",
+        "vovó zangada": "Todo dia você escolhe algum jogador que não poderá votar durante o dia.",
+        "bêbado": "Você está constantemente bêbado e não deve falar durante o jogo.",
+        "idiota": "Se a aldeia tentar te matar na votação, você não morrerá, mas perderá seu direito de votar.",
+        "pistoleiro": "Você têm duas balas que podem ser usadas para matar alguém. Os tiros são muito barulhentos, seu papel será revelado logo depois do primeiro tiro.",
+        "lobisomem": "Durante a noite, você acorda junto com seus amigos lobisomens e decide uma vítima para matar. Durante o dia, tenta parecer um aldeão comum para não ser morto.",
+        "lobo solitário": "Você é um lobisomem comum, exceto que você só ganhará se for o último lobisomem vivo.",
+        "filhote de lobisomem": "Você é um lobisomem normal, exceto que, se você morrer, os lobisomens matarão duas pessoas na próxima noite.",
+        "humano amaldiçoado": "Você é um aldeão normal até os lobisomens tentarem te matar. Caso aconteça, você se torna um lobisomem.",
+        "feiticeira": "A cada noite você seleciona um jogador para ver se ele é o vidente ou um lobisomem.",
+        "lobo alfa": "Apenas uma vez por jogo, você pode morder um jogador para ele virar um lobisomem.",
+        "assassino em série": "A cada noite você pode matar outro jogador. Se você for o último jogador vivo, você vence.",
+        "inquisidor": "Cada noite você pode selecionar um jogador. Se ele é o líder da seita ou parte de uma seita, o jogador morrerá.",
+        "sósia": "Na primeira noite você seleciona um jogador. Se esse jogador morrer, você tomará o seu papel.",
+        "líder de seita": "Toda noite você escolhe alguém para unir à sua seita. Quando todos os jogadores se unirem, você vence.",
+        "cupido": "Durante a primeira noite, você seleciona duas pessoas para formarem um casal. Esse casal só ganhará se eles forem as duas últimas pessoas vivas. Se uma pessoa do casal morrer em qualquer momento, a outra também morrerá.",
+        "depressivo": "Você está muito triste. Seu objetivo é ser morto pela aldeia. Se você for linchado pela aldeia, você vence.",
+        "necromante": "Uma vez por jogo, você poderá reviver um jogador morto.",
+        "piromaníaco": "Toda noite você seleciona dois jogadores para encharcar com gasolina ou queimar todos os jogadores já encharcados. Você ganha se você for o último jogador vivo. Você não pode ser morto pelos lobisomens.",
+        "presidente": "Você é o presidente! Todo mundo sabe quem você é. Se você morrer, a aldeia perderá."
+    };
+    
     
     const chancesPapeisConfig = {
         'aldeão': 10,
@@ -430,60 +475,58 @@ if (page === 'mestre') {
 
 if (page === 'mediador') {
         const modal = document.getElementById('modal');
-        const continuarBtnModal = document.getElementById('continuar-btn');  // Botão no modal
-        const conteudoPrincipal = document.getElementById('pagina-mediador');  // Conteúdo principal
+        const continuarBtnModal = document.getElementById('continuar-btn');
+        const conteudoPrincipal = document.getElementById('pagina-mediador');
         const nomeJogador = document.getElementById('nome-jogador');
         const papelJogador = document.getElementById('papel-jogador');
-        const imagemJogador = document.getElementById('imagem-jogador');  // Elemento para a imagem do jogador
+        const imagemJogador = document.getElementById('imagem-jogador');
         const jogadores = JSON.parse(localStorage.getItem('jogadores')) || [];
-        let posicaoAtual = parseInt(localStorage.getItem('posicaoAtual')) || 0;
+        let posicaoAtual = JSON.parse(localStorage.getItem('posicaoAtual')) || 0;
         const resultadoSorteio = JSON.parse(localStorage.getItem('resultadoSorteio')) || [];
     
-        // Função para exibir o jogador atual no modal
         const exibirJogadorModal = () => {
             const jogadorAtual = jogadores[posicaoAtual] || 'Nenhum jogador encontrado.';
             document.querySelector('.nome-pessoa .comando').textContent = `Passe o aparelho para: ${jogadorAtual}`;
         };
+
+        const proximoJogador = () => {
+            posicaoAtual = (posicaoAtual + 1) % jogadores.length;
+            localStorage.setItem('posicaoAtual', posicaoAtual);
+        };
     
-        // Função para atualizar o nome, papel e imagem do jogador no conteúdo principal
+        continuarBtnModal.addEventListener('click', () => {
+            
+            modal.style.display = 'none';
+    
+            setTimeout(() => {
+                atualizarJogadorInfo();
+                proximoJogador();
+            }, 500);
+            
+        });
+    
         const atualizarJogadorInfo = () => {
             const jogadorAtual = jogadores[posicaoAtual];
             const papelAtual = resultadoSorteio.find(jogador => jogador.jogador === jogadorAtual)?.papel || 'Sem papel definido';
             
-            nomeJogador.textContent = `Jogador: ${jogadorAtual}`;
-            papelJogador.textContent = `Papel: ${papelAtual}`;
+            nomeJogador.textContent = `${jogadorAtual}`;
+            papelJogador.textContent = `${papelAtual}`;
+
+            const descricao = descricaoPorPapel[papelAtual.toLowerCase()] || 'Nenhuma descrição disponível para este papel.';
+            document.getElementById('descricao-papel').textContent = descricao;
             
-            // Atualiza a imagem de acordo com o papel do jogador
             setTimeout(() => {
-                const imagemPapel = imagemPorPapel[papelAtual.toLowerCase()] || 'img/default.png';
-                imagemJogador.src = imagemPapel;  // Troca a imagem do jogador
-            }, 500);  // Atraso de 1 segundo para a troca da imagem
+                const imagemPapel = imagemPorPapel[papelAtual.toLowerCase()] || 'img/mestre.png';
+                imagemJogador.src = imagemPapel; 
+            
+            }, 50); 
+            
         };
     
-        // Atualiza o jogador no início do modal
         exibirJogadorModal();
         
-    
-        // Função para passar para o próximo jogador
-        const proximoJogador = () => {
-            posicaoAtual = (posicaoAtual + 1) % jogadores.length;
-            localStorage.setItem('posicaoAtual', posicaoAtual);
-            exibirJogadorModal(); // Atualiza o jogador no modal
-        };
-    
-        // Botão "Continuar" no modal
-        continuarBtnModal.addEventListener('click', () => {
-            
-            modal.style.display = 'none'; // Oculta o modal
-    
-            // Atualiza as informações do jogador após o modal (atrasa para garantir que o modal feche primeiro)
-            setTimeout(() => {
-                atualizarJogadorInfo();  // Atualiza a informação do jogador no conteúdo principal
-            }, 500);  // Atraso de 500ms
-        });
-
-        proximoJogador();
     }
+    
     
 
 
